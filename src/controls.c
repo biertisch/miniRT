15394bdde66cc94ab801461d76c7e2e97e85d200
biertisch <almeidarodrigues.beatriz@gmail.com>
2025-12-09 11:49:21 +0000

    minor adjustment

--- src/controls.c
+++ src/controls.c
@@ -6,7 +6,7 @@
 /*   By: beatde-a <beatde-a@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/05 11:58:02 by beatde-a          #+#    #+#             */
-/*   Updated: 2025/12/08 11:52:25 by beatde-a         ###   ########.fr       */
+/*   Updated: 2025/12/09 11:48:07 by beatde-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -237,6 +237,7 @@ static double	commit_slider(t_slider *slider, int range)
 	return (value);
 }
 
+// refactor next three functions to avoid repetition
 static void	transform_objects(t_slider **sliders, t_object **objects, int object_count)
 {
 	int	i;
@@ -249,7 +250,7 @@ static void	transform_objects(t_slider **sliders, t_object **objects, int object
 			objects[i]->geo.cylinder.center.x = commit_slider(&sliders[i][TRANSL_X], TL_RANGE);
 			objects[i]->geo.cylinder.center.y = commit_slider(&sliders[i][TRANSL_Y], TL_RANGE);
 			objects[i]->geo.cylinder.center.z = commit_slider(&sliders[i][TRANSL_Z], TL_RANGE);
-			// add rotation
+			// rotate axis vector along x, y, and z
 			objects[i]->geo.cylinder.radius = commit_slider(&sliders[i][RESIZE_D], RS_RANGE) / 2;
 			objects[i]->geo.cylinder.height = commit_slider(&sliders[i][RESIZE_H], RS_RANGE);
 		}
@@ -258,7 +259,6 @@ static void	transform_objects(t_slider **sliders, t_object **objects, int object
 			objects[i]->geo.sphere.center.x = commit_slider(&sliders[i][TRANSL_X], TL_RANGE);
 			objects[i]->geo.sphere.center.y = commit_slider(&sliders[i][TRANSL_Y], TL_RANGE);
 			objects[i]->geo.sphere.center.z = commit_slider(&sliders[i][TRANSL_Z], TL_RANGE);
-			// add rotation
 			objects[i]->geo.sphere.radius = commit_slider(&sliders[i][RESIZE_D], RS_RANGE) / 2;
 		}
 		else if (objects[i]->type == PLANE)
@@ -266,7 +266,7 @@ static void	transform_objects(t_slider **sliders, t_object **objects, int object
 			objects[i]->geo.plane.point.x = commit_slider(&sliders[i][TRANSL_X], TL_RANGE);
 			objects[i]->geo.plane.point.y = commit_slider(&sliders[i][TRANSL_Y], TL_RANGE);
 			objects[i]->geo.plane.point.z = commit_slider(&sliders[i][TRANSL_Z], TL_RANGE);
-			// add rotation
+			// rotate normal vector along x, y, and z
 		}
 		i++;
 	}
@@ -274,6 +274,7 @@ static void	transform_objects(t_slider **sliders, t_object **objects, int object
 
 static void	transform_camera(t_slider *sliders, t_camera *camera)
 {
+	//translations
 	camera->center.x = sliders[TRANSL_X].base_value + (sliders[TRANSL_X].knob_pos - 0.5) * TL_RANGE;
 	camera->center.y = sliders[TRANSL_Y].base_value + (sliders[TRANSL_Y].knob_pos - 0.5) * TL_RANGE;
 	camera->center.z = sliders[TRANSL_Z].base_value + (sliders[TRANSL_Z].knob_pos - 0.5) * TL_RANGE;
@@ -283,7 +284,9 @@ static void	transform_camera(t_slider *sliders, t_camera *camera)
 	sliders[TRANSL_Y].knob_pos = 0.5;
 	sliders[TRANSL_Z].base_value = camera->center.z;
 	sliders[TRANSL_Z].knob_pos = 0.5;
-	// add rotation
+
+	// rotate orientation vector along x, y, and z
+
 	// do I need camera_initialize?
 }
 
@@ -613,6 +616,7 @@ static void draw_value(t_panel *panel, int value, int y, int pos)
 	free(str);
 }
 
+// use RS_RANGE for resizing
 static void	render_values(t_panel *panel, t_world *scene, int slider, int y)
 {
 	int		curr;
@@ -631,11 +635,12 @@ static void	render_values(t_panel *panel, t_world *scene, int slider, int y)
 		min = curr - RT_RANGE / 2;
 	}
 	y += SLIDER_H + 3;
-	draw_value(panel, min, y, -1);
+	draw_value(panel, min, y, -1); // check return for failed malloc
 	draw_value(panel, curr, y, 0);
 	draw_value(panel, max, y, 1);
 }
 
+// refactor next three functions to avoid repetition
 static void	render_resizing(t_panel *panel, t_world *scene, int x, int y)
 {
 	int	obj;
@@ -708,6 +713,7 @@ static void	render_translation(t_panel *panel, t_world *scene, int x, int y)
 	}
 }
 
+// remove rotation from spheres and lights
 static void	render_sliders(t_panel *panel, t_world *scene)
 {
 	int	obj;
