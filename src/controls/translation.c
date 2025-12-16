deb66be59054446c8e4374e14a3d512b925304d7
biertisch <almeidarodrigues.beatriz@gmail.com>
2025-12-16 16:52:05 +0000

    control panel: corrected translation along x axis

--- src/controls/translation.c
+++ src/controls/translation.c
@@ -6,17 +6,19 @@
 /*   By: beatde-a <beatde-a@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 11:56:17 by beatde-a          #+#    #+#             */
-/*   Updated: 2025/12/16 16:28:31 by beatde-a         ###   ########.fr       */
+/*   Updated: 2025/12/16 16:50:05 by beatde-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "controls.h"
 
-static double	translate(t_slider *slider)
+static double	translate(t_slider *slider, double dir)
 {
 	double	value;
+	double	delta;
 
-	value = slider->base_value + (slider->knob_pos - 0.5) * RANGE_TR;
+	delta = (slider->knob_pos - 0.5) * RANGE_TR * dir;
+	value = slider->base_value + delta;
 	slider->base_value = value;
 	slider->knob_pos = 0.5;
 	return (value);
@@ -24,7 +26,7 @@ static double	translate(t_slider *slider)
 
 void	apply_translation(t_slider *sliders, t_vec3 *position)
 {
-	position->x = translate(&sliders[TRANSL_X]);
-	position->y = translate(&sliders[TRANSL_Y]);
-	position->z = translate(&sliders[TRANSL_Z]);
+	position->x = translate(&sliders[TRANSL_X], -1);
+	position->y = translate(&sliders[TRANSL_Y], 1);
+	position->z = translate(&sliders[TRANSL_Z], 1);
 }
