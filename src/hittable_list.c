38e1edd6c476274bfee06892ed931965c4456df1
biertisch <almeidarodrigues.beatriz@gmail.com>
2025-12-17 17:04:39 +0000

    corrected and integrated cone collision

--- src/hittable_list.c
+++ src/hittable_list.c
@@ -15,45 +15,58 @@ t_vec3	default_hitable_pdf_generate(t_pdf *self, t_vec3 origin)
 	return (new_vec3(1, 0, 0));
 }
 
+static int	assign_hit(t_object *new_object)
+{
+	if (new_object->type == SPHERE)
+		new_object->hit = sphere_hit;
+	else if (new_object->type == PLANE)
+		new_object->hit = plane_hit;
+	else if (new_object->type == CYLINDER)
+		new_object->hit = cylinder_hit;
+	else if (new_object->type == QUAD)
+		new_object->hit = quad_hit;
+	else if (new_object->type == CONE)
+		new_object->hit = cone_hit;
+	else
+		return (0);
+	return (1);
+}
+
+static int	assign_geometry(t_object *new_object, void *geo)
+{
+	if (new_object->type == SPHERE)
+		new_object->geo.sphere = *(t_sphere *)geo;
+	else if (new_object->type == PLANE)
+		new_object->geo.plane = *(t_plane *)geo;
+	else if (new_object->type == CYLINDER)
+		new_object->geo.cylinder = *(t_cylinder *)geo;
+	else if (new_object->type == QUAD)
+		new_object->geo.quad = *(t_quad *)geo;
+	else if (new_object->type == CONE)
+		new_object->geo.cone = *(t_cone *)geo;
+	else
+		return (0);
+	return (1);
+}
+
 void	add_object_to_world(t_world *world, t_geo_type geo_type, void *geo)
 {
 	t_object	*new_object;
 
 	if (world->num_objects >= 50)
 	{
-		printf("World object limit reached\n");
+		ft_putstr_fd("World object limit reached\n", STDERR_FILENO);
 		return ;
 	}
 	new_object = (t_object *)malloc(sizeof(t_object));
 	if (!new_object)
 		return ;
 	new_object->type = geo_type;
-	switch (geo_type)
+	if (!assign_geometry(new_object, geo) || !assign_hit(new_object))
 	{
-		case SPHERE:
-			new_object->geo.sphere = *(t_sphere *)geo;
-			new_object->hit = sphere_hit;
-			// new_object->bbox = new_object->geo.sphere.bbox;
-			break;
-		case PLANE:
-			new_object->geo.plane = *(t_plane *)geo;
-			new_object->hit = plane_hit;
-			// new_object->bbox = new_object->geo.plane.bbox;
-			break;
-		case CYLINDER:
-			new_object->geo.cylinder = *(t_cylinder *)geo;
-			new_object->hit = cylinder_hit;
-			// new_object->bbox = new_object->geo.cylinder.bbox;
-			break;
-		case QUAD:
-			new_object->geo.quad = *(t_quad *)geo;
-			new_object->hit = quad_hit;
-			// new_object->bbox = new_object->geo.quad.bbox;
-			break;
-		default:
-			free(new_object);
-			printf("Unsupport geometry type\n");
-			return ;
+		free(new_object);
+		ft_putstr_fd("Unsupport geometry type\n", STDERR_FILENO);
+		return ;
 	}
 	world->objects[world->num_objects] = new_object;
 	world->num_objects++;
@@ -73,7 +86,7 @@ int	world_hit(t_world *world, t_ray *ray, t_interval ray_t, t_hit_record *rec)
 	{
 		current = world->objects[i];
 		ray_t.max = closest_so_far;
-		
+
 		if (current->hit && current->hit(ray, ray_t, *current, &temp_rec))
 		{
 			hit_anything = 1;
@@ -98,7 +111,7 @@ int	world_hit_old(t_object *objects, t_ray *ray, t_interval ray_t, t_hit_record 
 	while (current != NULL)
 	{
 		ray_t.max = closest_so_far;
-		
+
 		if (current->hit && current->hit(ray, ray_t, *current, &temp_rec))
 		{
 			hit_anything = 1;
