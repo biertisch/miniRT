dd76860ebc627bca3f2336c8d16a46f0cd71931f
Your Name <your.email@example.com>
2025-12-18 10:09:03 +0000

    fit line counts

--- src/sphere.c
+++ src/sphere.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   sphere.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bliu <bliu@student.42lisboa.com>           +#+  +:+       +#+        */
+/*   By: bliu <bliu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 17:27:57 by bliu              #+#    #+#             */
-/*   Updated: 2025/12/17 17:31:26 by bliu             ###   ########.fr       */
+/*   Updated: 2025/12/18 10:07:55 by bliu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ void	set_face_normal(t_ray *ray, t_vec3 outward_normal, t_hit_record *record)
 	}
 }
 
-void	get_sphere_uv(t_vec3 p, double *u, double *v)
+static void	get_sphere_uv(t_vec3 p, double *u, double *v)
 {
 	double	theta;
 	double	phi;
@@ -47,19 +47,15 @@ void	get_sphere_uv(t_vec3 p, double *u, double *v)
 	*v = theta / M_PI;
 }
 
-int	sphere_hit(t_ray *ray, t_interval ray_t, t_object obj, t_hit_record *record)
+static int	root_calc(t_sphere *sphere, t_ray *ray, t_interval ray_t, double *root)
 {
 	t_vec3		oc;
 	double		a;
 	double		h;
 	double		c;
 	double		discriminant;
-	t_sphere	*sphere;
 	double		sqrtd;
-	double		root;
-	t_vec3		outward_normal;
 
-	sphere = &obj.geo.sphere;
 	oc = vec3_sub(sphere->center, ray->origin);
 	a = vec3_dot(ray->direction, ray->direction);
 	h = vec3_dot(oc, ray->direction);
@@ -68,13 +64,46 @@ int	sphere_hit(t_ray *ray, t_interval ray_t, t_object obj, t_hit_record *record)
 	if (discriminant < 0)
 		return (0);
 	sqrtd = sqrt(discriminant);
-	root = (h - sqrtd) / a;
-	if (!interval_surrounds(&ray_t, root))
+	*root = (h - sqrtd) / a;
+	if (!interval_surrounds(&ray_t, *root))
 	{
-		root = (h + sqrtd) / a;
-		if (!interval_surrounds(&ray_t, root))
+		*root = (h + sqrtd) / a;
+		if (!interval_surrounds(&ray_t, *root))
 			return (0);
 	}
+	return (1);
+}
+
+int	sphere_hit(t_ray *ray, t_interval ray_t, t_object obj, t_hit_record *record)
+{
+	// t_vec3		oc;
+	// double		a;
+	// double		h;
+	// double		c;
+	// double		discriminant;
+	t_sphere	*sphere;
+	// double		sqrtd;
+	double		root;
+	t_vec3		outward_normal;
+
+	sphere = &obj.geo.sphere;
+	// oc = vec3_sub(sphere->center, ray->origin);
+	// a = vec3_dot(ray->direction, ray->direction);
+	// h = vec3_dot(oc, ray->direction);
+	// c = vec3_dot(oc, oc) - (sphere->radius * sphere->radius);
+	// discriminant = h * h - a * c;
+	// if (discriminant < 0)
+	// 	return (0);
+	// sqrtd = sqrt(discriminant);
+	// root = (h - sqrtd) / a;
+	// if (!interval_surrounds(&ray_t, root))
+	// {
+	// 	root = (h + sqrtd) / a;
+	// 	if (!interval_surrounds(&ray_t, root))
+	// 		return (0);
+	// }
+	if (!root_calc(sphere, ray, ray_t, &root))
+		return (0);
 	record->t = root;
 	record->p = ray_at(ray, record->t);
 	outward_normal = vec3_mul_n(vec3_sub(record->p, sphere->center), 1.0 / sphere->radius);
