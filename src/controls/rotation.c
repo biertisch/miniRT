eec046b052a509419691e3e1206662e4906c6a1c
biertisch <almeidarodrigues.beatriz@gmail.com>
2025-12-18 21:52:41 +0000

    corrected object rotations

--- src/controls/rotation.c
+++ src/controls/rotation.c
@@ -6,57 +6,77 @@
 /*   By: beatde-a <beatde-a@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 11:55:23 by beatde-a          #+#    #+#             */
-/*   Updated: 2025/12/16 13:50:11 by beatde-a         ###   ########.fr       */
+/*   Updated: 2025/12/18 21:50:50 by beatde-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "controls.h"
 
-static void	rotate_z(t_vec3 *result, t_vec3 base, double c, double s)
+static t_vec3	rotate_z(t_vec3 base, double a)
 {
-	result->x = base.x * c - base.y * s;
-	result->y = base.x * s + base.y * c;
-	result->z = base.z;
-}
-
-static void	rotate_y(t_vec3 *result, t_vec3 base, double c, double s)
-{
-	result->x = base.x * c + base.z * s;
-	result->y = base.y;
-	result->z = -base.x * s + base.z * c;
-}
-
-static void	rotate_x(t_vec3 *result, t_vec3 base, double c, double s)
-{
-	result->x = base.x;
-	result->y = base.y * c - base.z * s;
-	result->z = base.y * s + base.z * c;
-}
-
-static t_vec3	rotate(t_slider *slider, t_vec3 base)
-{
-	t_vec3	result;
-	double	angle;
 	double	c;
 	double	s;
 
-	angle = (slider->knob_pos - 0.5) * RANGE_RT;
-	c = cos(angle);
-	s = sin(angle);
-	if (slider->type == ROTATE_X)
-		rotate_x(&result, base, c, s);
-	else if (slider->type == ROTATE_Y)
-		rotate_y(&result, base, c, s);
-	else if (slider->type == ROTATE_Z)
-		rotate_z(&result, base, c, s);
+	c = cos(a);
+	s = sin(a);
+	return ((t_vec3){
+		base.x * c - base.y * s,
+		base.x * s + base.y * c,
+		base.z
+	});
+}
+
+static t_vec3	rotate_y(t_vec3 base, double a)
+{
+	double	c;
+	double	s;
+
+	c = cos(a);
+	s = sin(a);
+	return ((t_vec3){
+		base.x * c + base.z * s,
+		base.y,
+		-base.x * s + base.z * c
+	});
+}
+
+static t_vec3	rotate_x(t_vec3 base, double a)
+{
+	double	c;
+	double	s;
+
+	c = cos(a);
+	s = sin(a);
+	return ((t_vec3){
+		base.x,
+		base.y * c - base.z * s,
+		base.y * s + base.z * c
+	});
+}
+
+// base_value is initialized to 0 for rotations
+static void	commit_rotation(t_slider *slider)
+{
+	double	delta;
+
+	delta = degrees_to_radians(-(slider->knob_pos - 0.5) * RANGE_RT);
+	slider->base_value += delta;
 	slider->knob_pos = 0.5;
-	result = vec3_normalize(result);
-	return (result);
 }
 
 void	apply_rotation(t_slider *sliders, t_vec3 *direction)
 {
-	*direction = rotate(&sliders[ROTATE_X], *direction);
-	*direction = rotate(&sliders[ROTATE_Y], *direction);
-	*direction = rotate(&sliders[ROTATE_Z], *direction);
+	t_vec3	tmp;
+	int		i;
+
+	tmp.x = sliders[ROTATE_X].initial_value;
+	tmp.y = sliders[ROTATE_Y].initial_value;
+	tmp.z = sliders[ROTATE_Z].initial_value;
+	i = ROTATE_X;
+	while (i <= ROTATE_Z)
+		commit_rotation(&sliders[i++]);
+	tmp = rotate_x(tmp, sliders[ROTATE_X].base_value);
+	tmp = rotate_y(tmp, sliders[ROTATE_Y].base_value);
+	tmp = rotate_z(tmp, sliders[ROTATE_Z].base_value);
+	*direction = vec3_normalize(tmp);
 }
