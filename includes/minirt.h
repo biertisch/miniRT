027ce30c5ed00d5c191de7c90747cf0fe05d882d
biertisch <almeidarodrigues.beatriz@gmail.com>
2025-12-23 15:08:53 +0000

    working on config files

--- includes/minirt.h
+++ includes/minirt.h
@@ -6,7 +6,7 @@
 /*   By: beatde-a <beatde-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/19 18:01:34 by bliu              #+#    #+#             */
-/*   Updated: 2025/12/23 10:14:24 by beatde-a         ###   ########.fr       */
+/*   Updated: 2025/12/23 15:06:03 by beatde-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@
 # include "parser.h"
 # include "controls.h"
 
-# define WIDTH 800
+# define WIDTH 600
 # define DEPTH 5
 # define RT_INFINITY 1e8
 # define ROT_SPEED 0.05
