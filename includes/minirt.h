17997e4f172a0b572d9414765e57ee6db0b68b0a
biertisch <almeidarodrigues.beatriz@gmail.com>
2025-12-23 14:16:39 +0000

    small adjustment

--- includes/minirt.h
+++ includes/minirt.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minirt.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: beatde-a <beatde-a@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: beatde-a <beatde-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/19 18:01:34 by bliu              #+#    #+#             */
-/*   Updated: 2025/12/22 10:46:52 by beatde-a         ###   ########.fr       */
+/*   Updated: 2025/12/23 10:14:24 by beatde-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@
 # include "parser.h"
 # include "controls.h"
 
-# define WIDTH 400
+# define WIDTH 800
 # define DEPTH 5
 # define RT_INFINITY 1e8
 # define ROT_SPEED 0.05
